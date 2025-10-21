ed5a7aa0401713c0f5ce13adda7876485c355266
shazamohamed705 <mshaza507@gmail.com>
2025-10-21 14:47:58 +0300

    1

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // تحسينات الأداء للبناء
+    minify: 'terser',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          pdf: ['html2pdf.js', 'jspdf', 'html2canvas']
+        }
+      }
+    }
+  },
   server: {
     proxy: {
       // Proxy external storage domain during dev to bypass CORS in html2canvas
